917320f9e1ab769cdf5209e76879c2b16d3732bf
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-25 09:52:28 +0000

    added code to compute pairwise similarities between hpo and snomed vectors

--- evaluate_similarities.py
+++ evaluate_similarities.py
@@ -1,3 +1,30 @@
-import sklearn
+from sklearn import metrics
+import pandas
 
-pairwise_distances = sklearn.metrics.pairwise.cosine_similarity(X=snomed_vectors, Y=hpo_vectors)
+
+def load_vec_file(path):
+    return pandas.read_csv(
+        path,
+        sep=' ',
+        index_col=0,
+        header=None
+    )
+
+
+def main():
+    hpo_vectors = load_vec_file('vectors/hpo_name_bert_vanilla_uncased_large.vec')
+    snomed_vectors = load_vec_file('vectors/snomed_name_bert_vanilla_uncased_large.vec')
+
+    # print(hpo_vectors.to_numpy())
+    # print(snomed_vectors)
+
+    pairwise_distances = metrics.pairwise.cosine_similarity(
+        X=snomed_vectors.to_numpy(copy=True),
+        Y=hpo_vectors.to_numpy(copy=True)
+    )
+
+    print(pairwise_distances.shape)
+
+
+if __name__ == '__main__':
+    main()
