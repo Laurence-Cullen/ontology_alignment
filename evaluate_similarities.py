07d1e8bd2e4079a9b9ee1b9ba68cfaaa22902a60
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-25 18:33:51 +0000

    able to extract most similar hpo terms for snomed term

--- evaluate_similarities.py
+++ evaluate_similarities.py
@@ -1,5 +1,7 @@
-from sklearn import metrics
+import numpy as np
 import pandas
+from sklearn import metrics
+from vec_builder import load_hpo_terms, build_hpo_snomed_map, build_snomed_hpo_map
 
 
 def load_vec_file(path):
@@ -23,7 +25,22 @@ def main():
         Y=hpo_vectors.to_numpy(copy=True)
     )
 
+    hpo_terms = load_hpo_terms()
+    hpo_snomed_map = build_hpo_snomed_map(hpo_terms)
+    snomed_hpo_terms = build_snomed_hpo_map(hpo_snomed_map)
+
     print(pairwise_distances.shape)
+
+    top_similarities_to_check = 10
+
+    # snomed_to_similar_hpo_terms = {}
+
+    for i in range(pairwise_distances.shape[0]):
+        max_indices = np.argpartition(pairwise_distances[i, :], -top_similarities_to_check)[-top_similarities_to_check:]
+
+
+    # print(max_indices)
+    # print(pairwise_distances[0, :][max_indices])
 
 
 if __name__ == '__main__':
