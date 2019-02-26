fcf6352b40e73f19fafbfbf665cd38d9b3b091de
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-26 22:59:22 +0000

    added file offset feature to corpus builder

--- build_training_corpus.py
+++ build_training_corpus.py
@@ -2,7 +2,7 @@ import pathlib
 import contextlib
 
 
-def build_corpus(target_dir, corpus_save_path):
+def build_corpus(target_dir, corpus_save_path, offset=0):
     paper_paths = pathlib.Path(target_dir).glob('**/*')
 
     files_written = 0
@@ -15,20 +15,21 @@ def build_corpus(target_dir, corpus_save_path):
             if files_written % 1000 == 0:
                 print(f'{files_written} files written')
 
-            with paper_path.open() as paper_file:
-                with contextlib.suppress(UnicodeDecodeError):
-                    for line in paper_file.readlines():
-                        if '==== Refs' in line:
-                            break
-                        elif '====' in line:
-                            continue
-                        elif len(line) < 3:
-                            corpus_file.write('\n')
-                        else:
-                            sentences = [sentence.strip(' \t\n\r') + '.\n' for sentence in line.split('.')]
-                            for sentence in sentences:
-                                if len(sentence) > 2:
-                                    corpus_file.write(sentence)
+            if files_written >= offset:
+                with paper_path.open() as paper_file:
+                    with contextlib.suppress(UnicodeDecodeError):
+                        for line in paper_file.readlines():
+                            if '==== Refs' in line:
+                                break
+                            elif '====' in line:
+                                continue
+                            elif len(line) < 3:
+                                corpus_file.write('\n')
+                            else:
+                                sentences = [sentence.strip(' \t\n\r') + '.\n' for sentence in line.split('.')]
+                                for sentence in sentences:
+                                    if len(sentence) > 2:
+                                        corpus_file.write(sentence)
 
             files_written += 1
 
@@ -37,7 +38,7 @@ def main():
     target_dir = 'medical_corpuses/comm'
     corpus_path = 'comm_corpus.txt'
 
-    build_corpus(target_dir=target_dir, corpus_save_path=corpus_path)
+    build_corpus(target_dir=target_dir, corpus_save_path=corpus_path, offset=380000)
 
 
 if __name__ == '__main__':
