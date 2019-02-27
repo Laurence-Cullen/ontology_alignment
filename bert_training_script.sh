edb1cf89f4a81d5220a0306f90b3a38b6314a9fd
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-27 13:58:53 +0000

    nearly functional training script!

--- bert_training_script.sh
+++ bert_training_script.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-python ./bert/run_pretraining.py \
+python run_pretraining.py \
     --input_file gs://bert-ontology/corpuses/*.txt \
-    --bert_config_file=gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_config.json \
+    --bert_config_file=uncased_L-24_H-1024_A-16/bert_config.json \
     --output_dir=gs://bert-ontology/models \
     --use_tpu=True \
-    --init_checkpoint gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_model.ckpt \
+    --init_checkpoint uncased_L-24_H-1024_A-16/bert_model.ckpt \
     --tpu_name node-1 \
     --do_train=True \
     --gcp_project iris-unmind \
-    --num_tpu_cores 2 \
+    --num_tpu_cores 8 \
+    --tpu_zone us-central1-c
+
 
