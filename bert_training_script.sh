fc7f087c551b652971227b0ceef1a4e18367228e
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-27 10:41:11 +0000

    bert training script update

--- bert_training_script.sh
+++ bert_training_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python run_pretraining.py \
-    --input_file sample_text.txt \
+    --input_file gs://medical-bert-fine-tuning/corpuses/*.txt \
     --bert_config_file=gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_config.json \
     --output_dir=gs://medical-bert-fine-tuning/models \
     --do_train=True
