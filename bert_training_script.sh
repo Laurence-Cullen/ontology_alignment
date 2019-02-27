5a2b9cd4f9e1dbbca3355a56096e2a34003cce80
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-27 15:42:33 +0000

    further tinkering with training script, still not working :(

--- bert_training_script.sh
+++ bert_training_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 python run_pretraining.py \
     --input_file gs://bert-ontology/corpuses/*.txt \
-    --bert_config_file=uncased_L-24_H-1024_A-16/bert_config.json \
+    --bert_config_file gs://bert-ontology/uncased_L-24_H-1024_A-16/bert_config.json \
     --output_dir=gs://bert-ontology/models \
     --use_tpu=True \
-    --init_checkpoint uncased_L-24_H-1024_A-16/bert_model.ckpt \
+    --init_checkpoint gs://bert-ontology/uncased_L-24_H-1024_A-16/bert_model.ckpt \
     --tpu_name node-1 \
     --do_train=True \
     --gcp_project iris-unmind \
