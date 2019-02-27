a42f108dc3068be08d61206f533b9b250acc1b89
Laurence Cullen <laurencesimoncullen@gmail.com>
2019-02-27 13:26:45 +0000

    tweak

--- bert_training_script.sh
+++ bert_training_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python bert/run_pretraining.py \
+python ./bert/run_pretraining.py \
     --input_file gs://bert-ontology/corpuses/*.txt \
     --bert_config_file=gs://bert_models/2018_10_18/uncased_L-12_H-768_A-12/bert_config.json \
     --output_dir=gs://bert-ontology/models \
